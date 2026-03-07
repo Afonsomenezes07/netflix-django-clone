f9991fcc4cf8f9f5b6a6fb51fc45bcfa369685b4
Afonso Menezes <afonsomenezes2000@gmail.com>
2026-03-07 20:50:38 -0300

    Create Movie model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+class Movie(models.Model):
+
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    thumbnail = models.ImageField(upload_to="thumbnails/")
+    video = models.FileField(upload_to="movies/")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
 # Create your models here.
