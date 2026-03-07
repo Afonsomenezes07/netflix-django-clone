c81b6dee2f20a06838e31edec5fcfab8305085f9
Afonso Menezes <afonsomenezes2000@gmail.com>
2026-03-07 20:26:31 -0300

    Register project apps in settings

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users",
+    "movies",
+    "categories",
+    "Watch_history",
 ]
 
 MIDDLEWARE = [
