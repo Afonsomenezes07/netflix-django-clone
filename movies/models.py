2ebe1d6ce19404787b5b87fb92aea542865da1e5
Afonso Menezes <afonsomenezes2000@gmail.com>
2026-03-10 16:28:58 -0300

    Add category relationship to Movie model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from categories.models import Category
+
+class Movie(models.Model):
+
+    title = models.CharField(max_length=200)
+
+    description = models.TextField()
+
+    thumbnail = models.ImageField(upload_to="thumbnails/")
+
+    video = models.FileField(upload_to="movies/")
+
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
 
 # Create your models here.
