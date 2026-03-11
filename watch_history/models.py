92bcb7b979914697f5dbd9cbef73e6c5d20d293b
Afonso Menezes <afonsomenezes2000@gmail.com>
2026-03-11 17:27:18 -0300

    Create WatchHistory model to track user progress

--- watch_history/models.py
+++ watch_history/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from movies.models import Movie
+
+
+class WatchHistory(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    progress = models.IntegerField(default=0)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.movie}"
 
 # Create your models here.
