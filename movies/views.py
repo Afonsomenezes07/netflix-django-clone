31caf9d33b2d8ea7c7d989692d194fca5a9849dd
Afonso Menezes <afonsomenezes2000@gmail.com>
2026-03-11 20:18:27 -0300

    Add search functionality to Movie API

--- movies/views.py
+++ movies/views.py
@@ -1,10 +1,15 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Movie
 from .serializers import MovieSerializer
 
 class MovieViewSet(viewsets.ModelViewSet):
 
     queryset = Movie.objects.all()
+
     serializer_class = MovieSerializer
 
+    filter_backends = [filters.SearchFilter]
+
+    search_fields = ['title']
+    
 # Create your views here.
