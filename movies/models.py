2fb90137ee645d781cca72e327544bd06749a8ae
Afonso Menezes <afonsomenezes2000@gmail.com>
2026-03-12 19:44:24 -0300

    Create Favorite model for user movie list

--- movies/models.py
+++ movies/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from categories.models import Category
+from django.contrib.auth.models import User
 
 class Movie(models.Model):
 
@@ -17,6 +18,16 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
-    
+
+class Favorite(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.movie}"
 
 # Create your models here.
