ff83cafb0aa470905902382c8d6ceda1a20d707f
Afonso Menezes <afonsomenezes2000@gmail.com>
2026-03-13 19:39:12 -0300

    Create endpoint to filter movies by category

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,10 @@
 from rest_framework import viewsets, filters
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from .models import Movie,Favorite
 from .serializers import MovieSerializer, FavoriteSerializer
+
 
 class MovieViewSet(viewsets.ModelViewSet):
 
@@ -12,10 +16,21 @@ class MovieViewSet(viewsets.ModelViewSet):
 
     search_fields = ['title']
 
+    @action(detail=False, methods=['get'])
+    def by_category(self, request):
+
+        category_id = request.query_params.get('category')
+
+        movies = Movie.objects.filter(category_id=category_id)
+
+        serializer = MovieSerializer(movies, many=True)
+
+        return Response(serializer.data)
+
 class FavoriteViewSet(viewsets.ModelViewSet):
 
-    queryset = Favorite.objects.all
+    queryset = Favorite.objects.all()
 
     serializer_class = FavoriteSerializer
-    
+
 # Create your views here.
