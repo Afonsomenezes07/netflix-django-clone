bbf29447d5c4828f2dc967385370a85654b6b7aa
Afonso Menezes <afonsomenezes2000@gmail.com>
2026-03-15 17:21:39 -0300

    Add JWT authentication endpoints

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,10 @@ from rest_framework.routers import DefaultRouter
 from movies.views import MovieViewSet
 from watch_history.views import WatchHistoryViewSet
 from movies.views import FavoriteViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = DefaultRouter()
 router.register(r"movies", MovieViewSet)
@@ -29,5 +33,7 @@ router.register(r"favorites", FavoriteViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/", include(router.urls)),
 ]
