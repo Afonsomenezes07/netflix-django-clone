a7ac96cb5aade0c15062f0ed86a428333a4c60a1
Afonso Menezes <afonsomenezes2000@gmail.com>
2026-03-15 18:41:39 -0300

    Create continue watching endpoint

--- watch_history/views.py
+++ watch_history/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from .models import WatchHistory
 from .serializers import WatchHistorySerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class WatchHistoryViewSet(viewsets.ModelViewSet):
@@ -8,5 +10,16 @@ class WatchHistoryViewSet(viewsets.ModelViewSet):
     queryset = WatchHistory.objects.all()
 
     serializer_class = WatchHistorySerializer
-    
+
+    @action(detail=False, methods=["get"])
+    def continue_watching(self, request):
+
+        user = request.user
+
+        history = WatchHistory.objects.filter(user=user)
+
+        serializer = WatchHistorySerializer(history, many=True)
+
+        return Response(serializer.data)
+
 # Create your views here.
