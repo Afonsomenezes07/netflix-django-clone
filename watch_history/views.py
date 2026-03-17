656828470c844e55e014910b2186b226a87d99a6
Afonso Menezes <afonsomenezes2000@gmail.com>
2026-03-17 11:55:22 -0300

    Add endpoint to save movie watching progress

--- watch_history/views.py
+++ watch_history/views.py
@@ -3,14 +3,18 @@ from .models import WatchHistory
 from .serializers import WatchHistorySerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 
 class WatchHistoryViewSet(viewsets.ModelViewSet):
 
-    queryset = WatchHistory.objects.all()
-
     serializer_class = WatchHistorySerializer
+    permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        return WatchHistory.objects.filter(user=self.request.user)
+
+    # 🎬 CONTINUE WATCHING
     @action(detail=False, methods=["get"])
     def continue_watching(self, request):
 
@@ -21,5 +25,23 @@ class WatchHistoryViewSet(viewsets.ModelViewSet):
         serializer = WatchHistorySerializer(history, many=True)
 
         return Response(serializer.data)
+
+    # ⏱️ SALVAR PROGRESSO
+    @action(detail=False, methods=["post"])
+    def save_progress(self, request):
+
+        user = request.user
+        movie_id = request.data.get("movie")
+        progress = request.data.get("progress")
+
+        history, created = WatchHistory.objects.get_or_create(
+            user=user,
+            movie_id=movie_id
+        )
+
+        history.progress = progress
+        history.save()
+
+        return Response({"status": "progress saved"})
 
 # Create your views here.
