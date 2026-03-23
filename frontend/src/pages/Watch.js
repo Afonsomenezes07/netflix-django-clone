bf54afe26b5a18a9ed2a1f2252296354d6d8473e
Afonso Menezes <afonsomenezes2000@gmail.com>
2026-03-22 21:16:04 -0300

    fix: correct template string in Watch.js API request

--- frontend/src/pages/Watch.js
+++ frontend/src/pages/Watch.js
@@ -7,7 +7,7 @@ function Watch() {
     const [movie, setMovie] = useState(null);
 
     useEffect(() => {
-        api.get('watch/${id}/').then((response) => {
+        api.get(`watch/${id}/`).then((response) => {
             setMovie(response.data);
         });
     }, [id]);
