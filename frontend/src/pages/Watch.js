facb5776062bffcb7a291bf169e9540d4fb05c9d
Afonso Menezes <afonsomenezes2000@gmail.com>
2026-03-22 21:06:48 -0300

    Create movie watch page with video player

---
+++ frontend/src/pages/Watch.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import api from "../services/api";
+import { useParams } from "react-router-dom";
+
+function Watch() {
+    const { id } = useParams();
+    const [movie, setMovie] = useState(null);
+
+    useEffect(() => {
+        api.get('watch/${id}/').then((response) => {
+            setMovie(response.data);
+        });
+    }, [id]);
+
+    if (!movie) return <div>Loading...</div>;
+
+    return (
+        <div>
+            <h1>{movie.title}</h1>
+
+            <video width="800" controls>
+                <source src={movie.video} type="video/mp4" />
+            </video>
+        </div>
+    );
+}
+
+export default Watch;
