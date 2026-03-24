4f24f4a1abddc003ac1adf356577bdffba88e4e5
Afonso Menezes <afonsomenezes2000@gmail.com>
2026-03-24 19:55:40 -0300

    Create login page and JWT authentication

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import api from "../services/api";
+
+function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async () => {
+        const response = await api.post("token/", {
+            username,
+            password,
+        });
+
+        localStorage.setItem("token", response.data.access);
+
+        window.location.href = "/";
+    };
+
+    return (
+        <div>
+            <h1>Login</h1>
+
+            <input
+            placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
+            />
+
+            <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <button onClick={handleLogin}>Entrar</button>
+        </div>
+    );
+}
+
+export default Login;
