0b0427fe1caa7e5cc64a2c0d56b755fa23637430
Afonso Menezes <afonsomenezes2000@gmail.com>
2026-03-24 21:03:06 -0300

    Add favorite button to movies

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,14 +1,25 @@
 import React, { useEffect, useState } from "react";
 import api from "../services/api";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
     const [data, setData] = useState({});
+    const navigate = useNavigate();
 
     useEffect(() => {
       api.get("home/").then((response) => {
         setData(response.data);
     });
 }, []);
+
+    const addToFavorites = async (movieId) => {
+        try {
+            await api.post("favorites/", { movie: movieId });
+            alert("Adicionado à Minha Lista!");
+        } catch (error) {
+            alert("Erro ao adicionar favorito");
+        }
+    };
 
     return (
         <div>
@@ -18,12 +29,23 @@ function Home() {
 
                 <div style={{ display: "flex", overflowX: "scroll"}}>
                     {data[category].map((movie) => (
-                        <img
-                        key={movie.id}
-                        src={movie.thumbnail}
-                        alt={movie.title}
-                        width="200"
-                        />
+                        <div key={movie.id} style={{ marginRight: "10px" }}>
+
+                            {/* CLICAR PARA ASSISTIR */}
+                            <img
+                                src={movie.thumbnail}
+                                alt={movie.title}
+                                width="200"
+                                style={{ cursor: "pointer" }}
+                                onClick={() => navigate(`/watch/${movie.id}`)}
+                            />
+
+                            {/* BOTÃO FAVORITO */}
+                            <button onClick={() => addToFavorites(movie.id)}>
+                                + Minha Lista
+                            </button>
+                            
+                        </div>
                     ))}
                 </div>
             </div>
