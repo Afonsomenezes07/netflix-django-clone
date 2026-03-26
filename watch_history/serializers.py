fb66582a7e12219cb4a290d6c94ae1dbcd6d1192
Afonso Menezes <afonsomenezes2000@gmail.com>
2026-03-25 22:00:51 -0300

    fix: return full movie object in WatchHistorySerializer to prevent frontend crash

--- watch_history/serializers.py
+++ watch_history/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from .models import WatchHistory
+from movies.serializers import MovieSerializer
 
 
 class WatchHistorySerializer(serializers.ModelSerializer):
 
+    movie = MovieSerializer(read_only=True)
+
     class Meta:
         model = WatchHistory
         fields = "__all__"
-        
